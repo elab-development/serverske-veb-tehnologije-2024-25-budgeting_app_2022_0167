006ad662357a6fe592cfbb12e059ae5946c538fd
vidanovicdj <vidanovicdj.65@gmail.com>
2025-10-11 15:23:22 +0200

    kreirani faktory za svaki model

---
+++ database/factories/ExpenseFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Expense;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ExpenseFactory extends Factory
+{
+    protected $model = Expense::class;
+
+    public function definition(): array
+    {
+        return [
+            'paid_by' => User::factory(),
+            'category_id' => Category::factory(),
+            'paid_at' => $this->faker->dateTimeBetween('-60 days', 'now'),
+            'amount' => $this->faker->randomFloat(2, 300, 15000),
+            'note' => $this->faker->optional()->sentence(6),
+        ];
+    }
+}
